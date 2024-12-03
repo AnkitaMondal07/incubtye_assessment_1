27c93db20dccc62c624347a6264a9edbe7c2116d
AnkitaMondal07 <ankitakartikmondal@gmail.com>
2024-12-03 19:56:47 +0530

    feat: handle ignore numbers greater than 1000

--- src/pages/StringCalculator.js
+++ src/pages/StringCalculator.js
@@ -4,17 +4,17 @@ export const add = (numbers) => {
     let delimiters = [",", "\n"];
     if (numbers.startsWith("//")) {
         const parts = numbers.split("\n");
-        delimiters.push(parts[0][2]);
+        delimiters.push(parts[0].slice(2));
         numbers = parts.slice(1).join("\n");
     }
 
     const regex = new RegExp(`[${delimiters.join("")}]`);
     const nums = numbers.split(regex).map((n) => parseInt(n, 10));
-    
+
     const negatives = nums.filter((n) => n < 0);
     if (negatives.length > 0) {
         throw new Error(`negative numbers not allowed: ${negatives.join(",")}`);
     }
 
-    return nums.reduce((sum, num) => sum + num, 0);
+    return nums.filter((n) => n <= 1000).reduce((sum, num) => sum + num, 0);
 };
